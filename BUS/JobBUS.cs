9d9eabc476a6e540e847a218180e0c53dd268ff0
Luan <nguyenhuuliem10@gmail.com>
2023-11-10 19:53:42 +0700

    time01

--- BUS/JobBUS.cs
+++ BUS/JobBUS.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace BUS
 {
-    internal class JobBUS
+    public class JobBUS
     {
+        public JobDAO jobDAO;
+        public JobBUS()
+        {
+            jobDAO = JobDAO.getInstance();
+        }
+
     }
+
 }
--- DAO/JobDAO.cs
+++ DAO/JobDAO.cs
@@ -1,6 +1,7 @@
 ﻿using DTO;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,33 @@ namespace DAO
 {
     public class JobDAO : InterfaceDAO<Job>
     {
+        public static JobDAO getInstance() 
+        {
+            return new JobDAO();
+        }
         public bool Add(Job t)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (SqlConnection connection = DbConnection.GetSqlConnection())
+                {
+                    string query = "INSERT INTO JOB (job_name,role, description)" +
+                        "VALLUE (@job_name, @role, @description)";
+                    using (SqlCommand comand=new SqlCommand(query, connection))
+                    {
+                        comand.Parameters.AddWithValue("@job_name", t.Job_Name);
+                        comand.Parameters.AddWithValue("@role", t.role);
+                        comand.Parameters.AddWithValue("@description", t.description);
+                        int rowsChanged = comand.ExecuteNonQuery();
+                        return rowsChanged > 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
         }
 
         public bool Delete(int id)
