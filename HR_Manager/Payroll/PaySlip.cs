10bb586ecfa2b0a72072dcb7a636922bb03a1a57
canhviet <canhviet@DESKTOP-BUDB9JC>
2023-11-10 09:57:05 +0700

    ok

---
+++ HR_Manager/Payroll/PaySlip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HR_Manager.Payroll
+{
+    public partial class PaySlip : UserControl
+    {
+        public PaySlip()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddPayslip addPayslip = new AddPayslip();
+            addPayslip.ShowDialog();
+        }
+    }
+}
