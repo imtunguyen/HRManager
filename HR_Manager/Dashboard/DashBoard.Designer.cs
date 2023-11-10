70871e28f0012d46fa469f7897b0759a3ce0b914
Luan <nguyenhuuliem10@gmail.com>
2023-11-10 13:37:38 +0700

    time01

--- HR_Manager/Dashboard/DashBoard.Designer.cs
+++ HR_Manager/Dashboard/DashBoard.Designer.cs
@@ -1,11 +1,23 @@
-﻿namespace HR_Manager
+﻿using System.Diagnostics.Contracts;
+using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Manager
 {
     partial class DashBoard
     {
         /// <summary> 
         /// Required designer variable.
         /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private IContainer components = null;
 
         /// <summary> 
         /// Clean up any resources being used.
@@ -28,18 +40,238 @@
         /// </summary>
         private void InitializeComponent()
         {
+            ComponentResourceManager resources = new ComponentResourceManager(typeof(DashBoard));
+            TabControl1 = new TabControl();
+            Employee = new TabPage();
+            Payroll = new TabPage();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            button1 = new Button();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            btnFind = new Button();
+            panel3 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            TabControl1.SuspendLayout();
+            Payroll.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
             SuspendLayout();
+            // 
+            // TabControl1
+            // 
+            TabControl1.Controls.Add(Employee);
+            TabControl1.Controls.Add(Payroll);
+            TabControl1.Dock = DockStyle.Fill;
+            TabControl1.Location = new Point(0, 0);
+            TabControl1.Margin = new Padding(3, 5, 3, 5);
+            TabControl1.Name = "TabControl1";
+            TabControl1.SelectedIndex = 0;
+            TabControl1.Size = new Size(1000, 600);
+            TabControl1.TabIndex = 0;
+            // 
+            // Employee
+            // 
+            Employee.Location = new Point(4, 29);
+            Employee.Margin = new Padding(3, 5, 3, 5);
+            Employee.Name = "Employee";
+            Employee.Padding = new Padding(3, 5, 3, 5);
+            Employee.Size = new Size(992, 567);
+            Employee.TabIndex = 2;
+            Employee.Text = "Employee";
+            Employee.UseVisualStyleBackColor = true;
+            // 
+            // Payroll
+            // 
+            Payroll.Controls.Add(tableLayoutPanel1);
+            Payroll.Location = new Point(4, 29);
+            Payroll.Margin = new Padding(3, 5, 3, 5);
+            Payroll.Name = "Payroll";
+            Payroll.Size = new Size(992, 567);
+            Payroll.TabIndex = 1;
+            Payroll.Text = "Payroll";
+            Payroll.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 0);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel3, 0, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Margin = new Padding(3, 4, 3, 4);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 26.3363762F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 73.66363F));
+            tableLayoutPanel1.Size = new Size(992, 567);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 70F));
+            tableLayoutPanel2.Controls.Add(flowLayoutPanel1, 0, 0);
+            tableLayoutPanel2.Controls.Add(panel1, 1, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(3, 4);
+            tableLayoutPanel2.Margin = new Padding(3, 4, 3, 4);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 105F));
+            tableLayoutPanel2.Size = new Size(986, 141);
+            tableLayoutPanel2.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(button1);
+            flowLayoutPanel1.Location = new Point(3, 3);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(289, 135);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Cursor = Cursors.Hand;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(129, 132);
+            button1.TabIndex = 0;
+            button1.Text = "To Excel";
+            button1.TextImageRelation = TextImageRelation.ImageAboveText;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.ColumnCount = 2;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.Controls.Add(panel3, 1, 0);
+            tableLayoutPanel3.Dock = DockStyle.Fill;
+            tableLayoutPanel3.Location = new Point(3, 153);
+            tableLayoutPanel3.Margin = new Padding(3, 4, 3, 4);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.Size = new Size(986, 410);
+            tableLayoutPanel3.TabIndex = 1;
+            tableLayoutPanel3.Paint += tableLayoutPanel3_Paint;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btnFind);
+            panel1.Controls.Add(textBox1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(298, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(685, 135);
+            panel1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            textBox1.Location = new Point(3, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(510, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // btnFind
+            // 
+            btnFind.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnFind.Cursor = Cursors.Hand;
+            btnFind.Location = new Point(538, 79);
+            btnFind.Name = "btnFind";
+            btnFind.Size = new Size(144, 53);
+            btnFind.TabIndex = 1;
+            btnFind.Text = "Tìm kiếm";
+            btnFind.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(label3);
+            panel3.Controls.Add(label2);
+            panel3.Controls.Add(label1);
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(496, 3);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(487, 404);
+            panel3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(27, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(159, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Employeer Cost";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(274, 27);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Anually";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(407, 26);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 20);
+            label3.TabIndex = 1;
+            label3.Text = "Monthly";
             // 
             // DashBoard
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ButtonHighlight;
+            Controls.Add(TabControl1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "DashBoard";
             Size = new Size(1000, 600);
+            TabControl1.ResumeLayout(false);
+            Payroll.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            flowLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel3.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+        private TabPage Employee;
+        private TabPage Payroll;
+        private TabControl TabControl1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private TableLayoutPanel tableLayoutPanel3;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button button1;
+        private Panel panel1;
+        private Button btnFind;
+        private TextBox textBox1;
+        private Panel panel3;
+        private Label label1;
+        private Label label3;
+        private Label label2;
     }
 }
--- HR_Manager/Dashboard/DashBoard.cs
+++ HR_Manager/Dashboard/DashBoard.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace HR_Manager
 {
@@ -15,6 +16,33 @@ namespace HR_Manager
         public DashBoard()
         {
             InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Choose File";
+            saveFileDialog.Filter = "Excel Document|*.xlsx";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                try
+                {
+                    MessageBox.Show("Export File succed");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error:" + ex.Message);
+                }
+            }
+        }
+
+        private void tableLayoutPanel3_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
