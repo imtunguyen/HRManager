14c17b12bdcd4221b9eca01f6f3138d5d5299ced
Khoa <ankhoa.dev@gmail.com>
2023-11-21 22:48:08 +0700

    Job Detail

--- HR_Manager/Employee/JobDetailUserControl.cs
+++ HR_Manager/Employee/JobDetailUserControl.cs
@@ -290,7 +290,7 @@ namespace HR_Manager.Employee
 					DateTime toDateJobDetail = jobDetail.ToDate.Date;
 
 					// Kiểm tra xem khoảng thời gian từ fromDate đến toDate nằm trong khoảng thời gian có trong hợp đồng
-					bool isWithinRange = fromDatePicker >= fromDateJobDetail && toDatePicker <= toDateJobDetail;
+					bool isWithinRange = fromDatePicker <= fromDateJobDetail && toDatePicker <= toDateJobDetail;
 					if (isWithinRange)
 					{
 						result.Add(jobDetail);
@@ -343,7 +343,7 @@ namespace HR_Manager.Employee
 					DateTime toDateJobDetail = jobDetail.ToDate.Date;
 
 					// Kiểm tra xem khoảng thời gian từ fromDate đến toDate nằm trong khoảng thời gian có trong hợp đồng
-					bool isWithinRange = fromDatePicker >= fromDateJobDetail && toDatePicker <= toDateJobDetail;
+					bool isWithinRange = fromDatePicker <= fromDateJobDetail && toDatePicker <= toDateJobDetail;
 					if (isWithinRange)
 					{
 						result.Add(jobDetail);
@@ -361,6 +361,17 @@ namespace HR_Manager.Employee
 		private void dtpNgayBatDau_ValueChanged(object sender, EventArgs e)
 		{
 			filterDate();
+		}
+
+		private void txtTimKiem_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (e.KeyChar == (char)Keys.Enter)
+			{
+				// Enter key is pressed
+				// Add your logic here
+				TimKiem();
+				e.Handled = true; // This line prevents the beep sound
+			}
 		}
 	}
 }
