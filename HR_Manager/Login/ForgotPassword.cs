6f0f6bc6c686b07fd26d02a5234f57387a261384
Khoa <ankhoa.dev@gmail.com>
2023-11-28 06:59:43 +0700

    Add Enter Event

--- HR_Manager/Login/ForgotPassword.cs
+++ HR_Manager/Login/ForgotPassword.cs
@@ -13,63 +13,103 @@ using System.Windows.Forms;
 
 namespace HR_Manager
 {
-    public partial class ForgotPassword : Form
-    {
-        private Account accDTO;
-        private AccountBUS accBUS;
-        private List<Account> accList;
-        public ForgotPassword()
-        {
-            accDTO = new Account();
-            accBUS = new AccountBUS();
-            accList = new List<Account>();
-            InitializeComponent();
-        }
+	public partial class ForgotPassword : Form
+	{
+		private Account accDTO;
+		private AccountBUS accBUS;
+		private List<Account> accList;
+		public ForgotPassword()
+		{
+			accDTO = new Account();
+			accBUS = new AccountBUS();
+			accList = new List<Account>();
+			InitializeComponent();
+		}
 
-        public object MessageIcons { get; private set; }
-        private void txtUsername_TextChanged(object sender, EventArgs e)
-        {
-            errorPro.SetError(txtUsername, "Username does not exist!");
-            errorPro.SetError(txtUsername, string.Empty);
-        }
-        private void btnSubmit_Click(object sender, EventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(txtUsername.Text) || string.IsNullOrWhiteSpace(txtNewPass.Text) || string.IsNullOrWhiteSpace(txtConfirm.Text))
-            {
-                MessageBox.Show("Please fill in the required information!", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-            else
-            {
-                string username = txtUsername.Text;
-                string newPass = txtNewPass.Text;
-                string confirm = txtConfirm.Text;
-                accList = accBUS.GetUserName(username);
-                if (accList.Count > 0)
-                {
-                    if (newPass != confirm)
-                    {
-                        // Hiển thị thông báo lỗi nếu mật khẩu không khớp
-                        errorPro.SetError(txtConfirm, "Password incorrect!");
-                    }
-                    else
-                    {
-                        // Xóa thông báo lỗi trước đó
-                        errorPro.SetError(txtConfirm, string.Empty);
+		public object MessageIcons { get; private set; }
+		private void txtUsername_TextChanged(object sender, EventArgs e)
+		{
+			errorPro.SetError(txtUsername, "Username does not exist!");
+			errorPro.SetError(txtUsername, string.Empty);
+		}
+		static bool AreStringsEqual(string str1, string str2)
+		{
+			return string.Equals(str1, str2, StringComparison.Ordinal);
+		}
+		private void btnSubmit_Click(object sender, EventArgs e)
+		{
+			if (string.IsNullOrWhiteSpace(txtUsername.Text) || string.IsNullOrWhiteSpace(txtNewPass.Text) || string.IsNullOrWhiteSpace(txtConfirm.Text))
+			{
+				MessageBox.Show("Please fill in the required information!", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				return;
+			}
+			else
+			{
+				string username = txtUsername.Text;
+				string newPass = txtNewPass.Text;
+				string confirm = txtConfirm.Text;
+				bool areEqual = AreStringsEqual(username, "admin");
+				if (!areEqual)
+				{
+					errorPro.SetError(txtUsername, "Username does not exist!");
+					return;
+				}
+				accList = accBUS.GetUserName(username);
+				if (accList.Count > 0)
+				{
+					if (newPass != confirm)
+					{
+						// Hiển thị thông báo lỗi nếu mật khẩu không khớp
+						errorPro.SetError(txtConfirm, "Password incorrect!");
+					}
+					else
+					{
+						// Xóa thông báo lỗi trước đó
+						errorPro.SetError(txtConfirm, string.Empty);
 
-                        accBUS.UpdatePassword(username, newPass);
-                        MessageBox.Show("Password changed successfully!", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        this.Close();
-                    }
-                }
-                else
-                {
-                    // Hiển thị thông báo lỗi nếu tên người dùng không tồn tại
-                    errorPro.SetError(txtUsername, "Username does not exist!");
-                }
-            }
-        }
+						accBUS.UpdatePassword(username, newPass);
+						MessageBox.Show("Password changed successfully!", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+						this.Close();
+					}
+				}
+				else
+				{
+					// Hiển thị thông báo lỗi nếu tên người dùng không tồn tại
+					errorPro.SetError(txtUsername, "Username does not exist!");
+				}
+			}
+		}
 
-        
-    }
+		private void ForgotPassword_Load(object sender, EventArgs e)
+		{
+			txtUsername.Focus();
+		}
+
+		private void txtUsername_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				e.SuppressKeyPress = true;  // Hủy sự kiện xuống dòng
+				txtNewPass.Focus();
+			}
+		}
+
+		private void txtNewPass_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				e.SuppressKeyPress = true;  // Hủy sự kiện xuống dòng
+				txtConfirm.Focus();
+			}
+		}
+
+		private void txtConfirm_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				e.SuppressKeyPress = true;  // Hủy sự kiện xuống dòng
+				btnSubmit.PerformClick();
+			}
+		}
+	}
 }
