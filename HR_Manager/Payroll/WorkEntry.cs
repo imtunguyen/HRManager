c757f2880713307f055f388569954cdc49dde05e
canhviet <canhviet@DESKTOP-BUDB9JC>
2023-11-28 10:21:16 +0700

    fix layout workentry

--- HR_Manager/Payroll/WorkEntry.cs
+++ HR_Manager/Payroll/WorkEntry.cs
@@ -12,123 +12,123 @@ using System.Windows.Forms;
 
 namespace HR_Manager.Employee
 {
-	public partial class WorkEntry : UserControl
-	{
-		int month, year;
-		public static int static_month, static_year;
-		public WorkEntry()
-		{
-			InitializeComponent();
-		}
+    public partial class WorkEntry : UserControl
+    {
+        int month, year;
+        public static int static_month, static_year;
+        public WorkEntry()
+        {
+            InitializeComponent();
+        }
 
-		private void WorkEntry_Load(object sender, EventArgs e)
-		{
-			render();
-		}
+        private void WorkEntry_Load(object sender, EventArgs e)
+        {
+            render();
+        }
 
-		private void render()
-		{
-			DateTime now = DateTime.Now;
-			month = now.Month;
-			year = now.Year;
+        private void render()
+        {
+            DateTime now = DateTime.Now;
+            month = now.Month;
+            year = now.Year;
 
-			string monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
-			lblDate.Text = monthname + " " + year;
+            string monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            lblDate.Text = monthname + " " + year;
 
-			static_month = month;
-			static_year = year;
+            static_month = month;
+            static_year = year;
 
-			DateTime startOfTheMonth = new DateTime(year, month, 1);
+            DateTime startOfTheMonth = new DateTime(year, month, 1);
 
-			int days = DateTime.DaysInMonth(year, month);
+            int days = DateTime.DaysInMonth(year, month);
 
-			int dayOfTheWeek = Convert.ToInt32(startOfTheMonth.DayOfWeek.ToString("d")) + 1;
+            int dayOfTheWeek = Convert.ToInt32(startOfTheMonth.DayOfWeek.ToString("d")) + 1;
 
-			for (int i = 1; i < dayOfTheWeek; i++)
-			{
-				UserControlBlank ucblank = new UserControlBlank();
-				dayContainer.Controls.Add(ucblank);
-			}
-			for (int i = 1; i <= days; i++)
-			{
-				UserControlDay ucdays = new UserControlDay();
-				ucdays.days(i);
-				dayContainer.Controls.Add(ucdays);
-			}
-		}
+            for (int i = 1; i < dayOfTheWeek; i++)
+            {
+                UserControlBlank ucblank = new UserControlBlank();
+                dayContainer.Controls.Add(ucblank);
+            }
+            for (int i = 1; i <= days; i++)
+            {
+                UserControlDay ucdays = new UserControlDay();
+                ucdays.days(i);
+                dayContainer.Controls.Add(ucdays);
+            }
+        }
 
-		private void button2_Click(object sender, EventArgs e)
-		{
-			dayContainer.Controls.Clear();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dayContainer.Controls.Clear();
 
-			month++;
+            month++;
 
-			if (month > 12)
-			{
-				month = 1;
-				year++;
-			}
+            if (month > 12)
+            {
+                month = 1;
+                year++;
+            }
 
-			string monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
-			lblDate.Text = monthname + " " + year;
+            string monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            lblDate.Text = monthname + " " + year;
 
-			static_month = month;
-			static_year = year;
+            static_month = month;
+            static_year = year;
 
-			DateTime startOfTheMonth = new DateTime(year, month, 1);
+            DateTime startOfTheMonth = new DateTime(year, month, 1);
 
-			int days = DateTime.DaysInMonth(year, month);
+            int days = DateTime.DaysInMonth(year, month);
 
-			int dayOfTheWeek = Convert.ToInt32(startOfTheMonth.DayOfWeek.ToString("d")) + 1;
+            int dayOfTheWeek = Convert.ToInt32(startOfTheMonth.DayOfWeek.ToString("d")) + 1;
 
-			for (int i = 1; i < dayOfTheWeek; i++)
-			{
-				UserControlBlank ucblank = new UserControlBlank();
-				dayContainer.Controls.Add(ucblank);
-			}
-			for (int i = 1; i <= days; i++)
-			{
-				UserControlDay ucdays = new UserControlDay();
-				ucdays.days(i);
-				dayContainer.Controls.Add(ucdays);
-			}
-		}
+            for (int i = 1; i < dayOfTheWeek; i++)
+            {
+                UserControlBlank ucblank = new UserControlBlank();
+                dayContainer.Controls.Add(ucblank);
+            }
+            for (int i = 1; i <= days; i++)
+            {
+                UserControlDay ucdays = new UserControlDay();
+                ucdays.days(i);
+                dayContainer.Controls.Add(ucdays);
+            }
+        }
 
-		private void button1_Click(object sender, EventArgs e)
-		{
-			dayContainer.Controls.Clear();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dayContainer.Controls.Clear();
 
-			month--;
+            month--;
 
-			if (month < 1)
-			{
-				year--;
-				month = 12;
-			}
+            if (month < 1)
+            {
+                year--;
+                month = 12;
+            }
 
-			string monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
-			lblDate.Text = monthname + " " + year;
+            string monthname = DateTimeFormatInfo.CurrentInfo.GetMonthName(month);
+            lblDate.Text = monthname + " " + year;
 
-			static_month = month;
-			static_year = year;
+            static_month = month;
+            static_year = year;
 
-			DateTime startOfTheMonth = new DateTime(year, month, 1);
+            DateTime startOfTheMonth = new DateTime(year, month, 1);
 
-			int days = DateTime.DaysInMonth(year, month);
+            int days = DateTime.DaysInMonth(year, month);
 
-			int dayOfTheWeek = Convert.ToInt32(startOfTheMonth.DayOfWeek.ToString("d")) + 1;
+            int dayOfTheWeek = Convert.ToInt32(startOfTheMonth.DayOfWeek.ToString("d")) + 1;
 
-			for (int i = 1; i < dayOfTheWeek; i++)
-			{
-				UserControlBlank ucblank = new UserControlBlank();
-				dayContainer.Controls.Add(ucblank);
-			}
-			for (int i = 1; i <= days; i++)
-			{
-				UserControlDay ucdays = new UserControlDay();
-				ucdays.days(i);
-				dayContainer.Controls.Add(ucdays);
-			}
-		}
-	}
+            for (int i = 1; i < dayOfTheWeek; i++)
+            {
+                UserControlBlank ucblank = new UserControlBlank();
+                dayContainer.Controls.Add(ucblank);
+            }
+            for (int i = 1; i <= days; i++)
+            {
+                UserControlDay ucdays = new UserControlDay();
+                ucdays.days(i);
+                dayContainer.Controls.Add(ucdays);
+            }
+        }
+    }
 }
